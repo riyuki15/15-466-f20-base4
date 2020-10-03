794f4ad479b3d3fb6f73c56607f1c154891edfdf
Michelle Chang <mychang@andrew.cmu.edu>
2020-10-03 11:06:10 -0700

    navigates among scenes

--- demo_menu.cpp
+++ demo_menu.cpp
@@ -14,8 +14,10 @@
 //});
 
 std::shared_ptr< MenuMode > demo_menu;
+std::vector< std::shared_ptr< MenuMode > > menuModes;
 MenuMode::Menus menus;
-int scene_count = 2;
+// TODO: remember to change this when adding/removing a text scene!!
+int scene_count = 3;
 
 void load_text_scenes() {
   for (int i = 0; i < scene_count; i++) {
@@ -46,36 +48,51 @@ void go_to_next_scene(int choice) {
 
 Load< void > load_demo_menu(LoadTagDefault, []() {
   load_text_scenes();
-  std::vector< MenuMode::Item > items;
-  MenuMode::TextScene cur = menus.textScenes[0];
-  items.emplace_back(cur.text);
-  for (int i = 0; i < cur.choice_count; i++) {
-    items.emplace_back(cur.choices_text[i]);
+  for (int j = 0; j < menus.scene_count; j++) {
+    std::shared_ptr< MenuMode > cur_menu;
+    std::vector< MenuMode::Item > items;
+    MenuMode::TextScene cur = menus.textScenes[j];
+    items.emplace_back(cur.text);
+    for (int i = 0; i < cur.choice_count; i++) {
+      items.emplace_back(cur.choices_text[i]);
+      items.back().on_select = [](MenuMode::Item const&) {
+          Mode::set_current(std::make_shared< PlayMode >());
+      };
+    }
+    /*items.emplace_back("lighting - forward");
     items.back().on_select = [](MenuMode::Item const&) {
-        Mode::set_current(std::make_shared< PlayMode >());
+      Mode::set_current(std::make_shared< DemoLightingForwardMode >());
     };
+    items.emplace_back("lighting - deferred");
+    items.back().on_select = [](MenuMode::Item const&) {
+      Mode::set_current(std::make_shared< DemoLightingDeferredMode >());
+    };*/
+
+
+    cur_menu = std::make_shared< MenuMode >(items);
+    cur_menu->selected = 1;
+    //demo_menu->atlas = trade_font_atlas;
+    cur_menu->view_min = glm::vec2(0.0f, 0.0f);
+    cur_menu->view_max = glm::vec2(320.0f, 200.0f);
+
+    cur_menu->layout_items(2.0f);
+
+    //demo_menu->left_select = &trade_font_atlas->lookup(">");
+    cur_menu->left_select_offset = glm::vec2(-5.0f - 3.0f, 0.0f);
+    //demo_menu->right_select = &trade_font_atlas->lookup("<");
+    cur_menu->right_select_offset = glm::vec2(0.0f + 3.0f, 0.0f);
+    cur_menu->select_bounce_amount = 5.0f;
+    menuModes.emplace_back(cur_menu);
   }
-	/*items.emplace_back("lighting - forward");
-	items.back().on_select = [](MenuMode::Item const&) {
-		Mode::set_current(std::make_shared< DemoLightingForwardMode >());
-	};
-	items.emplace_back("lighting - deferred");
-	items.back().on_select = [](MenuMode::Item const&) {
-		Mode::set_current(std::make_shared< DemoLightingDeferredMode >());
-	};*/
+  for (int l = 0; l < scene_count; l++) {
+    for (int m = 0; m < menuModes[l]->items.size(); m++) {
+      if (menuModes[l]->items[m].name.compare(menus.textScenes[l].text) == 0) continue;
+      int next_scene = menus.textScenes[l].next_scenes[m - 1];
+      menuModes[l]->items[m].on_select = [next_scene](MenuMode::Item const&) {
+          Mode::set_current(menuModes[next_scene]);
+      };
+    }
+  }
+  demo_menu = menuModes[0];
 
-
-	demo_menu = std::make_shared< MenuMode >(items);
-	demo_menu->selected = 1;
-	//demo_menu->atlas = trade_font_atlas;
-	demo_menu->view_min = glm::vec2(0.0f, 0.0f);
-	demo_menu->view_max = glm::vec2(320.0f, 200.0f);
-
-	demo_menu->layout_items(2.0f);
-
-	//demo_menu->left_select = &trade_font_atlas->lookup(">");
-	demo_menu->left_select_offset = glm::vec2(-5.0f - 3.0f, 0.0f);
-	//demo_menu->right_select = &trade_font_atlas->lookup("<");
-	demo_menu->right_select_offset = glm::vec2(0.0f + 3.0f, 0.0f);
-	demo_menu->select_bounce_amount = 5.0f;
 });
